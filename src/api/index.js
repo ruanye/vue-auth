c40052f6a3644113794fdb5f9882a985b15701a2
ruanye <55343581@qq.com>
2019-11-25 10:12:06 +0800

    一级路由过滤

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+axios.interceptors.response.use(
+  res => res.data,
+  err => Promise.reject(err),
+);
 // 获取权限列表的接口
 export const getAuth = () => axios.get('/auth');
 
--- src/store/moudle/route.js
+++ src/store/moudle/route.js
@@ -1,6 +1,17 @@
 // store/moudle/route.js 管理路由的vuex
 import { getAuth } from '../../api';
-
+// 引入我们的动态路由列表
+import { routerMap } from '../../router';
+// 过滤需要路由的方法
+// eslint-disable-next-line no-unused-vars
+function getAccesRouterList(routes, authlist) {
+  return routes.filter((route) => {
+    if (authlist.rules.page[route.name]) {
+      return true;
+    }
+    return false;
+  });
+}
 const state = {};
 const actions = {
   async getAuthRoute() {
@@ -9,6 +20,8 @@ const actions = {
       // eslint-disable-next-line no-unused-vars
       const authList = await getAuth();
       // 根据权限列表来过滤出我需要的路由列表
+      // eslint-disable-next-line no-unused-vars
+      const needRouter = getAccesRouterList(routerMap, authList);
     } catch (e) {
       console.log(e);
     }
